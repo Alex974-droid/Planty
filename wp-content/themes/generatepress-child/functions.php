6f3dd6e4c310ce0318fd7eb558e7eabeb1d190bf
Alexandre CATHERINE <Alexandre@imacdealexandre.home>
2025-01-12 19:37:18 +0400

    ETAPE_3_Thème enfant_avec header_footer_functions_style css_OK

--- wp-content/themes/generatepress-child/functions.php
+++ wp-content/themes/generatepress-child/functions.php
@@ -1,11 +1,32 @@
 <?php
-/**
- * Générer les styles du thème parent.
- */
-function generatepress_child_enqueue_styles() {
+// Charger les styles du thème parent et du thème enfant
+add_action('wp_enqueue_scripts', 'generatepress_child_enqueue_styles');
+
+function generatepress_child_enqueue_styles()
+/*{
+    // Charger le style du thème parent
     wp_enqueue_style(
-        'generatepress-parent-style',
-        get_template_directory_uri() . '/style.css'
+        'parent-style',
+        get_template_directory_uri() . '/style.css',
+        array(),
+        filemtime(get_template_directory() . '/style.css')
+    );
+
+    // Charger le style du thème enfant (dans /css/generatepress.css)
+    wp_enqueue_style(
+        'child-style',
+        get_stylesheet_directory_uri() . '/css/generatepress.css',
+        array('parent-style'),
+        filemtime(get_stylesheet_directory() . '/css/generatepress.css') 
     );
 }
-add_action( 'wp_enqueue_scripts', 'generatepress_child_enqueue_styles' );
+*/
+{
+    // Chargement du style.css du thème parent et enfant
+    wp_enqueue_style('parent-style', get_template_directory_uri() . '/style.css');
+    wp_enqueue_style('child-style', get_stylesheet_directory_uri() . '/style.css', array(), filemtime(get_stylesheet_directory() . '/style.css'));
+
+
+}
+
+
